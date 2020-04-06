8f278fb12089ada50aa5e9995ea4574d5a5fa9d1
Spencer Mogil <smogil12@gmail.com>
2020-04-06 14:18:42 -0400

    style: remove hashtags under img cards

--- frontend/kick-covid-19/src/components/Home.js
+++ frontend/kick-covid-19/src/components/Home.js
@@ -16,17 +16,6 @@ function Home() {
             exercitationem praesentium nihil.
           </p>
         </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-            #winter
-          </span>
-        </div>
       </div>
       <div className="max-w-sm rounded overflow-hidden shadow-lg ml-8">
         <img
@@ -41,17 +30,6 @@ function Home() {
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
             exercitationem praesentium nihil.
           </p>
-        </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-            #winter
-          </span>
         </div>
       </div>
     </div>
