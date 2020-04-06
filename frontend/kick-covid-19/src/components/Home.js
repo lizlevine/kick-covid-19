2d3e9dd8433733764a5b95fa5ba258c77b9807dc
Spencer Mogil <smogil12@gmail.com>
2020-04-06 00:26:41 -0400

    style: add background color to cards

--- frontend/kick-covid-19/src/components/Home.js
+++ frontend/kick-covid-19/src/components/Home.js
@@ -1,16 +1,16 @@
 import React from "react";
 function Home() {
   return (
-    <div className="flex justify-center mt-8 mb-4">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="flex justify-center mt-8 mb-4 ">
+      <div className="max-w-sm  rounded overflow-hidden shadow-lg">
         <img
           className="w-full"
           src="https://tailwindcss.com/img/card-top.jpg"
           alt="Sunset in the mountains"
         />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">
+        <div className="px-6 py-4 bg-gray-400  ">
+          <div className="font-bold text-xl mb-2 ">The Coldest Sunset</div>
+          <p className="text-gray-700  text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
             exercitationem praesentium nihil.
@@ -34,7 +34,7 @@ function Home() {
           src="https://tailwindcss.com/img/card-top.jpg"
           alt="Sunset in the mountains"
         />
-        <div className="px-6 py-4">
+        <div className="px-6 py-4 bg-gray-400 ">
           <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
           <p className="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
