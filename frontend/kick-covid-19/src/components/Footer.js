9cf11119228f352c613ba727f88784e8722e1063
Spencer Mogil <54917317+smogil12@users.noreply.github.com>
2020-04-07 17:54:48 -0400

    feat: footer links working, final step would be adding in social and github links. can do later on (#9)

--- frontend/kick-covid-19/src/components/Footer.js
+++ frontend/kick-covid-19/src/components/Footer.js
@@ -3,56 +3,38 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-200 mt-12">
       <div className="max-w-screen-xl mx-auto py-8 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center">
           <div className="px-5 py-2">
-            <a
-              href="#"
-              className="text-base leading-6 text-gray-500 hover:text-gray-900"
-            >
-              About
-            </a>
+            <Link to="/">
+              <a
+                href="#"
+                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+              >
+                Home
+              </a>
+            </Link>
           </div>
           <div className="px-5 py-2">
-            <a
-              href="#"
-              className="text-base leading-6 text-gray-500 hover:text-gray-900"
-            >
-              Blog
-            </a>
+            <Link to="/blog">
+              <a
+                href="#"
+                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+              >
+                Blog
+              </a>
+            </Link>
           </div>
           <div className="px-5 py-2">
-            <a
-              href="#"
-              className="text-base leading-6 text-gray-500 hover:text-gray-900"
-            >
-              Jobs
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a
-              href="#"
-              className="text-base leading-6 text-gray-500 hover:text-gray-900"
-            >
-              Press
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a
-              href="#"
-              className="text-base leading-6 text-gray-500 hover:text-gray-900"
-            >
-              Accessibility
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a
-              href="#"
-              className="text-base leading-6 text-gray-500 hover:text-gray-900"
-            >
-              Partners
-            </a>
+            <Link to="/resources">
+              <a
+                href="#"
+                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+              >
+                Resources
+              </a>
+            </Link>
           </div>
         </nav>
         <div className="mt-8 flex justify-center">
@@ -102,11 +84,6 @@ function Footer() {
               />
             </svg>
           </a>
-        </div>
-        <div className="mt-8">
-          <p className="text-center text-base leading-6 text-gray-400">
-            &copy; 2020 Workflow, Inc. All rights reserved.
-          </p>
         </div>
       </div>
     </div>
