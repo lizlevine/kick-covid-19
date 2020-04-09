b8fc9eade2352440d6b02439ece356551877d848
Tuscany <lizlevine.rigie@gmail.com>
2020-04-09 10:35:38 -0400

    chore: Refactor some code on post.controllers.js to create proper relationship btwn collections

--- controllers/post.contollers.js
+++ controllers/post.contollers.js
@@ -1,27 +1,26 @@
-const db = require('../models');
-const crudControllers = require('../utils/crudControllers');
+const db = require("../models");
+const crudControllers = require("../utils/crudControllers");
 
 const controllers = crudControllers(db.post);
 
 //Needs Review
 controllers.getAnswers = async (req, res) => {
-    const post = await db.post.findAll({ where: { id: req.params.id } });
-    const answers = await post.getAnswers();
-    res.json(answers);
-   
+  const post = await db.post.findById(req.params.id).populate("answers");
+  console.log(post);
+  const answers = post.answers;
+  res.json(answers);
 };
 
 controllers.getAll = async (req, res) => {
-// res.send("test received");
-}
+  // res.send("test received");
+};
 
-//Needs Review 
+//Needs Review
 controllers.addAnswer = async (req, res) => {
-    const { text } = req.body;
-    const post = await db.post.findAll({ where: { id: req.params.id } });
-    const result = await post.createAnswer({ text });
-    res.json(result); 
+  const { text } = req.body;
+  const post = await db.post.find({ where: { id: req.params.id } });
+  const result = await post.createAnswer({ text });
+  res.json(result);
 };
 
 module.exports = controllers;
-
