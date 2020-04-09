b2a9f46ada08354fc01a1d3a1affafc310abfaea
Tuscany <lizlevine.rigie@gmail.com>
2020-04-09 15:40:20 -0400

    repushing

--- server.js
+++ server.js
@@ -18,17 +18,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes"));
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'))
-})
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
 
 // If deployed, use the deployed database. Otherwise use the local kickCovidUsers database
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/kickCovidUsers";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
 
 // localhost listening
 app.listen(PORT, function () {
