556b676fb8db488ce742a2b8342275cc1c53ce4c
Spencer Mogil <smogil12@gmail.com>
2020-04-10 18:46:24 -0400

    chore: add prop

--- frontend/kick-covid-19/src/components/Post.js
+++ frontend/kick-covid-19/src/components/Post.js
@@ -4,12 +4,7 @@ const Post = ({ body }) => (
   <div class="shadow p-4 bg-white">
     <div class="text-left">
       <h3 class="mb-2 text-gray-700">Card Title</h3>
-      <p class="text-grey-600 text-sm">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem.{" "}
-      </p>
+      <p class="text-grey-600 text-sm">{body}</p>
     </div>
     ​
   </div>
