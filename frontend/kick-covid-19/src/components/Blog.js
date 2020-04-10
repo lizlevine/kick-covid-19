d4237c34c98e354f4b976ccbf9b8506169b4400c
Spencer Mogil <smogil12@gmail.com>
2020-04-10 19:13:46 -0400

    chore: commit, blog broken but troubleshooting

--- frontend/kick-covid-19/src/components/Blog.js
+++ frontend/kick-covid-19/src/components/Blog.js
@@ -2,6 +2,27 @@ import React from "react";
 import axios from "axios";
 import Post from "./Post";
 
-const Blog = () => <Post />;
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get("/api/posts");
+    const { data: posts } = response;
+    this.setState({ posts });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.state.posts.map(({ body }) => (
+          <Post body={body} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Blog;
