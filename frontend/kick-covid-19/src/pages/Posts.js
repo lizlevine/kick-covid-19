a672fb8c780fa578b77ef1503da82abdb929fabd
Spencer Mogil <smogil12@gmail.com>
2020-04-12 23:02:31 -0400

    feat: appending post by id to single view page

--- frontend/kick-covid-19/src/pages/Posts.js
+++ frontend/kick-covid-19/src/pages/Posts.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import Post from "../components/Post";
+import { Link } from "react-router-dom";
 
-function Posts() {
-  const { id } = useParams();
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+  }
 
-  return <h1>Post {id}</h1>;
+  async componentDidMount() {
+    const response = await axios.get("/api/posts/:id").then((response) => {
+      console.log(response);
+      this.setState((state) => {
+        return { posts: state.posts.concat(response.data) };
+      });
+    });
+    // const { data: posts } = response;
+    // this.setState({ posts });
+  }
+
+  render() {
+    console.log(
+      `[DEBUG] this.state.posts = ${JSON.stringify(this.state.posts, null, 2)}`
+    );
+    return (
+      <div className="row">
+        {this.state.posts.map(({ _id, body, answers }) => (
+          <Link key={_id} to={`/posts/${_id}`}>
+            <Post body={body} />
+          </Link>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Posts;
