c6c3a024c79f7d4ba4e07061919c8156cc16cdb8
Spencer Mogil <54917317+smogil12@users.noreply.github.com>
2020-04-14 19:51:22 -0400

    feat: add button where users will add posts (#23)

--- frontend/kick-covid-19/src/pages/Blog.js
+++ frontend/kick-covid-19/src/pages/Blog.js
@@ -23,12 +23,19 @@ class Blog extends React.Component {
       `[DEBUG] this.state.posts = ${JSON.stringify(this.state.posts, null, 2)}`
     );
     return (
-      <div className="row">
-        {this.state.posts.map(({ _id, body, answers }) => (
-          <Link key={_id} to={`/posts/${_id}`}>
-            <Post body={body} />
-          </Link>
-        ))}
+      <div className="maindiv">
+        <div className="buttons flex justify-center">
+          <button className="mb-3 mt-3 flex items-center  rounded-full shadow bg-teal-500 px-4 py-2 text-white hover:bg-blue-400">
+            Make a Post
+          </button>
+        </div>
+        <div className="row ">
+          {this.state.posts.map(({ _id, body, answers }) => (
+            <Link key={_id} to={`/posts/${_id}`}>
+              <Post body={body} />
+            </Link>
+          ))}
+        </div>
       </div>
     );
   }
