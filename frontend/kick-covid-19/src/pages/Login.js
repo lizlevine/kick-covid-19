74b64aab088ddcc3152a0346297e7e468ff24bef
Tuscany <lizlevine.rigie@gmail.com>
2020-04-15 14:28:29 -0400

    feat: Add style to forms - WIP- first pass

--- frontend/kick-covid-19/src/pages/Login.js
+++ frontend/kick-covid-19/src/pages/Login.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "./style.css";
 
 function Login() {
   return (
     <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        id="login"
+      >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             for="username"
           >
-            Username
+            Email
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -26,24 +30,23 @@ function Login() {
             Password
           </label>
           <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            // className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
-            placeholder="******************"
+            placeholder="Password"
           />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
+          <p className="text-purple-500 text-xs italic">Password required.</p>
         </div>
         <div className="flex items-center justify-between">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
-            Sign In
+            Sign-In
           </button>
           <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            className="inline-block align-baseline font-bold text-sm text-purple-500 hover:text-blue-800"
             href="#"
           >
             Forgot Password?
--- frontend/kick-covid-19/src/pages/SignUp.js
+++ frontend/kick-covid-19/src/pages/SignUp.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "./style.css";
 
 function SignUp() {
   return (
-    <form class="w-full max-w-sm">
+    <form className="w-full max-w-sm">
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-username"
+          >
+            Email
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-username"
+            type="password"
+            placeholder=""
+          />
+        </div>
+      </div>
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/3">
           <label
@@ -17,7 +36,7 @@ function SignUp() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-full-name"
             type="text"
-            value=""
+            placeholder=""
           />
         </div>
       </div>
@@ -35,7 +54,7 @@ function SignUp() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-username"
             type="password"
-            placeholder="******************"
+            placeholder=""
           />
         </div>
       </div>
