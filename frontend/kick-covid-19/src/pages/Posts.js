3a4a3527e14ec584704d93c5ae425db5901c7ba1
Spencer Mogil <smogil12@gmail.com>
2020-04-16 17:57:02 -0400

    chore: saving for review

--- frontend/kick-covid-19/src/pages/Posts.js
+++ frontend/kick-covid-19/src/pages/Posts.js
@@ -22,7 +22,7 @@ class Posts extends React.Component {
     const { body } = this.state;
     console.log("fired submit");
     return axios.post("/api/answers", {
-      body: "hopefully by summer",
+      body: "no one knows!",
       post: "5e965b1ea3f63120cbca6881",
     });
     // .then((res) => {
