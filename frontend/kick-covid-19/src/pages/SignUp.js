2e3dd5a6a990173533b5a154f9f30190c67e63b1
Tuscany <lizlevine.rigie@gmail.com>
2020-04-16 12:23:09 -0400

    feat: add padding to blog pageand margin to forms

--- frontend/kick-covid-19/src/pages/SignUp.js
+++ frontend/kick-covid-19/src/pages/SignUp.js
@@ -4,72 +4,72 @@ import "./style.css";
 function SignUp() {
   return (
     <form className="w-full max-w-sm">
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
           <label
-            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             for="inline-username"
           >
             Email
           </label>
         </div>
-        <div class="md:w-2/3">
+        <div className="md:w-2/3">
           <input
-            class="bg-gray-200 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
+            className="bg-gray-200 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
             id="inline-username"
             type="password"
             placeholder=""
           />
         </div>
       </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
           <label
-            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             for="inline-full-name"
           >
             User Name
           </label>
         </div>
-        <div class="md:w-2/3">
+        <div className="md:w-2/3">
           <input
-            class="bg-gray-200 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
+            className="bg-gray-200 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
             id="inline-full-name"
             type="text"
             placeholder=""
           />
         </div>
       </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
           <label
-            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             for="inline-username"
           >
             Password
           </label>
         </div>
-        <div class="md:w-2/3">
+        <div className="md:w-2/3">
           <input
-            class="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
+            className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
             id="inline-username"
             type="password"
             placeholder=""
           />
         </div>
       </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3"></div>
-        <label class="md:w-2/3 block text-gray-500 font-bold">
-          <input class="mr-2 leading-tight" type="checkbox" />
-          <span class="text-sm">I'd like to receive updates</span>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3"></div>
+        <label className="md:w-2/3 block text-gray-500 font-bold">
+          <input className="mr-2 leading-tight" type="checkbox" />
+          <span className="text-sm">I'd like to receive updates</span>
         </label>
       </div>
-      <div class="md:flex md:items-center">
-        <div class="md:w-1/3"></div>
-        <div class="md:w-2/3">
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
           <button
-            class="shadow bg-teal-400 hover:bg-teal-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            className="shadow bg-teal-400 hover:bg-teal-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="button"
           >
             Sign Up
