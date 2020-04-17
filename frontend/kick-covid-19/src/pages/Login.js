f745c1aacabef6a5202d60577855fdf75ea76c14
Tuscany <lizlevine.rigie@gmail.com>
2020-04-16 22:38:24 -0400

    chore: Fix form input fields to text

--- frontend/kick-covid-19/src/pages/Login.js
+++ frontend/kick-covid-19/src/pages/Login.js
@@ -17,7 +17,7 @@ function Login() {
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-400"
             id="inline-username"
-            type="password"
+            type="text"
             placeholder=""
           />
         </div>
