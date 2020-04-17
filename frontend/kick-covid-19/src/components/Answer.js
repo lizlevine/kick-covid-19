ad7d3f354df24b5b29a2492ae68b25e715b08ce3
Spencer Mogil <smogil12@gmail.com>
2020-04-17 00:30:37 -0400

    style: add answer component and add color to it

---
+++ frontend/kick-covid-19/src/components/Answer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Answer(props) {
+  return (
+    <div className="shadow p-4 bg-pink-100 mt-4">
+      <div className="text-left">
+        <h3 className="mb-2 text-gray-700">Card Title</h3>
+        <p className="text-grey-600 text-sm">{props.body}</p>
+      </div>
+      ​
+    </div>
+  );
+}
+export default Answer;
