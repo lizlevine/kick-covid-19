61e727fec7a6e149afc2abea980d4977d68a1aa6
Tuscany <lizlevine.rigie@gmail.com>
2020-04-17 15:33:52 -0400

    feat: Add usernames to posts and answers

--- frontend/kick-covid-19/src/pages/Posts.js
+++ frontend/kick-covid-19/src/pages/Posts.js
@@ -61,7 +61,7 @@ class Posts extends React.Component {
               <h1 className="mt-4 font-bold">Answers</h1>
               {this.state.post &&
                 this.state.post.answers.map((answer) => (
-                  <Answer body={answer.body} />
+                  <Answer user={answer.user} body={answer.body} />
                 ))}{" "}
             </React.Fragment>
           ) : (
