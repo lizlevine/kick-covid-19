85ad33e5a5fd33d098d709b3bd9f2cca4cb5f985
Spencer Mogil <smogil12@gmail.com>
2020-04-17 00:35:57 -0400

    style: add background hover styles to post and answers

--- frontend/kick-covid-19/src/components/Answer.js
+++ frontend/kick-covid-19/src/components/Answer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Answer(props) {
   return (
-    <div className="shadow p-4 bg-pink-100 mt-4">
+    <div className="shadow p-4 bg-pink-100 hover:bg-pink-200 mt-4">
       <div className="text-left">
         <h3 className="mb-2 text-gray-700">Card Title</h3>
         <p className="text-grey-600 text-sm">{props.body}</p>
