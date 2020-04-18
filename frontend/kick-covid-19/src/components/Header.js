bb06c3d842c92a2414c72df0abe5cb15b417a7d6
Spencer Mogil <smogil12@gmail.com>
2020-04-18 01:29:31 -0400

    style: add icon

--- frontend/kick-covid-19/src/components/Header.js
+++ frontend/kick-covid-19/src/components/Header.js
@@ -3,15 +3,11 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const Header = () => (
   <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
     <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <svg
-        className="fill-current h-8 w-8 mr-2"
-        width="54"
-        height="54"
-        viewBox="0 0 54 54"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-      </svg>
+      <img
+        class="h-10 w-10 mr-4 object-cover"
+        src="https://assets.website-files.com/5d9ba0eb5f6edb77992a99d0/5e62506c4527700a4546c461_iconfinder_doctor-advise-warning-suggestion-avatar_5728189.png"
+        alt=""
+      />
       <span className="font-semibold text-xl tracking-tight">Kick Covid</span>
     </div>
     <div className="block lg:hidden">
