691de9b469c558546dcec5f9e7045243a62dc8b5
Spencer Mogil <smogil12@gmail.com>
2020-04-18 12:46:50 -0400

    repush up authclient code

--- frontend/kick-covid-19/src/utils/api-client/auth.client.js
+++ frontend/kick-covid-19/src/utils/api-client/auth.client.js
@@ -1,24 +1,24 @@
-// module.exports = (apiClient) => ({
-//     login: async ({ email, password }) => {
-//         const {
-//             data: { token, user },
-//         } = await apiClient.post('/auth/login', {
-//             email,
-//             password,
-//         });
-//         localStorage.setItem(process.env.REACT_APP_TOKEN_LOCAL_STORAGE_KEY, token);
-//         return user;
-//     },
-//     signup: ({ email, password }) =>
-//         apiClient.post('/users', { email, password }),
-//     logout: () =>
-//         localStorage.removeItem(process.env.REACT_APP_TOKEN_LOCAL_STORAGE_KEY),
-//     me: () => {
-//         const token = localStorage.getItem('__kickcovid19_token');
-//         return apiClient.get('/auth/me', {
-//             headers: {
-//                 Authorization: `Bearer ${token}`,
-//             },
-//         });
-//     },
-// });
+module.exports = (apiClient) => ({
+  login: async ({ email, password }) => {
+    const {
+      data: { token, user },
+    } = await apiClient.post("/auth/login", {
+      email,
+      password,
+    });
+    localStorage.setItem(process.env.REACT_APP_TOKEN_LOCAL_STORAGE_KEY, token);
+    return user;
+  },
+  signup: ({ email, password }) =>
+    apiClient.post("/users", { email, password }),
+  logout: () =>
+    localStorage.removeItem(process.env.REACT_APP_TOKEN_LOCAL_STORAGE_KEY),
+  me: () => {
+    const token = localStorage.getItem("__kickcovid19_token");
+    return apiClient.get("/auth/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+});
