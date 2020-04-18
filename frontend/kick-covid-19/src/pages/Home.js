c56fac1401b29cd95b1ee92f41d1b997ade30cb4
Ben Florentino <florentinob92@gmail.com>
2020-04-17 23:42:00 -0400

    Feat: Added Twitter Feed

--- frontend/kick-covid-19/src/pages/Home.js
+++ frontend/kick-covid-19/src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Card from "../components/Card";
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import "./style.css";
 
 function Home() {
   return (
+    <div className="Twitter">
     <div className="Spencer">
-      <Card />
+      <Card />  
+    </div>
+    <div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="CDCgov"
+        options={{height: 400}}
+      />
+    </div>
     </div>
   );
 }
