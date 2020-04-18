556ed4599a3322b40e62b3653c49f80d336f401a
Spencer Mogil <54917317+smogil12@users.noreply.github.com>
2020-04-18 13:33:54 -0400

    Commentout authfor heroku (#48)
    * commented out code multiple files bc they broke heroku
    
    * chore: commentout code
    
    * commit for heroku

--- frontend/kick-covid-19/src/context/auth.context.js
+++ frontend/kick-covid-19/src/context/auth.context.js
@@ -1,78 +1,77 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-import apiClient from '../utils/api-client';
+// import apiClient from '../utils/api-client';
 
+// const AuthContext = React.createContext();
+// AuthContext.displayName = 'AuthContext';
 
-const AuthContext = React.createContext();
-AuthContext.displayName = 'AuthContext';
+// function AuthProvider(props) {
+//     const [user, setUser] = useState(null);
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [isSuccess, setIsSuccess] = useState(true);
 
-function AuthProvider(props) {
-    const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isSuccess, setIsSuccess] = useState(true);
+//     const login = React.useCallback(
+//         (creds) => {
+//             setIsLoading(true);
+//             apiClient.auth
+//                 .login(creds)
+//                 .then((user) => {
+//                     setIsLoading(false);
+//                     setIsSuccess(true);
+//                     setUser(user || null);
+//                 })
+//                 .catch(() => setIsLoading(false));
+//         },
+//         [setUser, setIsLoading, setIsSuccess]
+//     );
 
-    const login = React.useCallback(
-        (creds) => {
-            setIsLoading(true);
-            apiClient.auth
-                .login(creds)
-                .then((user) => {
-                    setIsLoading(false);
-                    setIsSuccess(true);
-                    setUser(user || null);
-                })
-                .catch(() => setIsLoading(false));
-        },
-        [setUser, setIsLoading, setIsSuccess]
-    );
+//     const signup = React.useCallback(
+//         (creds) => {
+//             apiClient.auth
+//                 .signup(creds)
+//                 .then(({ data }) => {
+//                     setIsLoading(false);
+//                     setIsSuccess(true);
+//                     setUser(data.user || null);
+//                 })
+//                 .catch(() => setIsLoading(false));
+//         },
+//         [setUser]
+//     );
 
-    const signup = React.useCallback(
-        (creds) => {
-            apiClient.auth
-                .signup(creds)
-                .then(({ data }) => {
-                    setIsLoading(false);
-                    setIsSuccess(true);
-                    setUser(data.user || null);
-                })
-                .catch(() => setIsLoading(false));
-        },
-        [setUser]
-    );
+//     const logout = React.useCallback(() => {
+//         apiClient.auth.logout();
+//         setUser(null);
+//     },  [setUser]);
 
-    const logout = React.useCallback(() => {
-        apiClient.auth.logout();
-        setUser(null);
-    },  [setUser]);
+//     useEffect(() => {
+//         setIsLoading(true);
+//         apiClient.auth
+//             .me()
+//             .then(({ data }) => {
+//                 setIsLoading(false);
+//                 setIsSuccess(true);
+//                 setUser(data.user || null);
+//             })
+//             .catch(() => {
+//                 setIsLoading(false);
+//             });
+//     },  [setUser, setIsLoading, setIsSuccess]);
 
-    useEffect(() => {
-        setIsLoading(true);
-        apiClient.auth
-            .me()
-            .then(({ data }) => {
-                setIsLoading(false);
-                setIsSuccess(true);
-                setUser(data.user || null);
-            })
-            .catch(() => {
-                setIsLoading(false);
-            });
-    },  [setUser, setIsLoading, setIsSuccess]);
-
-    if (isLoading) {
-        return <h1>App loading...</h1>;
-    }
-    if (isSuccess) {
-        return (
-            <AuthContext.Provider
-                value={{ user, login, signup, logout }}
-                {...props}
-            />    
-        );
-    }
-    return <h1>Error loading the app: </h1>;
-}
-export function useAuth() {
-    return React.useContext(AuthContext);
-}
-export default AuthProvider;
+//     if (isLoading) {
+//         return <h1>App loading...</h1>;
+//     }
+//     if (isSuccess) {
+//         return (
+//             <AuthContext.Provider
+//                 value={{ user, login, signup, logout }}
+//                 {...props}
+//             />
+//         );
+//     }
+//     return <h1>Error loading the app: </h1>;
+// }
+// export function useAuth() {
+//     return React.useContext(AuthContext);
+// }
+// export default AuthProvider;
--- frontend/kick-covid-19/src/utils/api-client/index.js
+++ frontend/kick-covid-19/src/utils/api-client/index.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(process.env.REACT_APP_TOKEN_LOCAL_STORAGE_KEY)}`;
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+// axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(process.env.REACT_APP_TOKEN_LOCAL_STORAGE_KEY)}`;
 
-export default {
-    auth: require('./auth.client')(axios),
-};
+// export default {
+//     auth: require('./auth.client')(axios),
+// };
